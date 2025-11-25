97b8fee86152bc533f019cc962fb8d0fae36a8a1
Gobi Mahato <thegobimahato@gmail.com>
2025-11-25 14:07:29 +0530

    improve the landing page deisgn & add motion animation in auth pages

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -17,66 +19,83 @@ import { loginFn } from '@/lib/auth'
 import { cn } from '@/lib/utils'
 import { useRouter } from '@tanstack/react-router'
 
+import { motion } from 'motion/react'
+
 type LoginFormProps = {
   switchToSignUp: () => void
 }
 
 export function LoginForm({ switchToSignUp }: LoginFormProps) {
   const router = useRouter()
+
   return (
     <div className={cn('flex flex-col gap-6 max-w-xl mx-auto p-12')}>
-      <Card>
-        <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form
-            onSubmit={async (e) => {
-              e.preventDefault()
-              const formData = new FormData(e.target as HTMLFormElement)
-              await loginFn({
-                data: {
-                  email: formData.get('email') as string,
-                  password: formData.get('password') as string,
-                },
-              })
-              router.invalidate()
-            }}
-          >
-            <FieldGroup>
-              <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </Field>
-              <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                </div>
-                <Input id="password" name="password" type="password" required />
-              </Field>
-              <Field>
-                <Button type="submit">Login</Button>
+      {/* whole-card animation */}
+      <motion.div
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 30, opacity: 0 }}
+        transition={{ duration: 1, ease: 'easeOut' }}
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
 
-                <FieldDescription className="text-center">
-                  Don&apos;t have an account?{' '}
-                  <button onClick={switchToSignUp} className="underline">
-                    Sign up
-                  </button>
-                </FieldDescription>
-              </Field>
-            </FieldGroup>
-          </form>
-        </CardContent>
-      </Card>
+          <CardContent>
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault()
+                const formData = new FormData(e.target as HTMLFormElement)
+                await loginFn({
+                  data: {
+                    email: formData.get('email') as string,
+                    password: formData.get('password') as string,
+                  },
+                })
+                router.invalidate()
+              }}
+            >
+              <FieldGroup>
+                <Field>
+                  <FieldLabel htmlFor="email">Email</FieldLabel>
+                  <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </Field>
+                <Field>
+                  <div className="flex items-center">
+                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                  </div>
+                  <Input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                  />
+                </Field>
+                <Field>
+                  <Button type="submit">Login</Button>
+
+                  <FieldDescription className="text-center">
+                    Don&apos;t have an account?{' '}
+                    <button onClick={switchToSignUp} className="underline">
+                      Sign up
+                    </button>
+                  </FieldDescription>
+                </Field>
+              </FieldGroup>
+            </form>
+          </CardContent>
+        </Card>
+      </motion.div>
     </div>
   )
 }
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -17,65 +19,83 @@ import { signUpFn } from '@/lib/auth'
 import { cn } from '@/lib/utils'
 import { useRouter } from '@tanstack/react-router'
 
+import { motion } from 'motion/react'
+
 type SignUpFormProps = {
   switchToLogIn: () => void
 }
+
 export function SignUpForm({ switchToLogIn }: SignUpFormProps) {
   const router = useRouter()
+
   return (
     <div className={cn('flex flex-col gap-6 max-w-xl mx-auto p-12')}>
-      <Card>
-        <CardHeader>
-          <CardTitle>Create a new account</CardTitle>
-          <CardDescription>
-            Enter your email below to create your new account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form
-            onSubmit={async (e) => {
-              e.preventDefault()
-              const formData = new FormData(e.target as HTMLFormElement)
-              await signUpFn({
-                data: {
-                  email: formData.get('email') as string,
-                  password: formData.get('password') as string,
-                },
-              })
-              router.invalidate()
-            }}
-          >
-            <FieldGroup>
-              <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </Field>
-              <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                </div>
-                <Input id="password" name="password" type="password" required />
-              </Field>
-              <Field>
-                <Button type="submit">Login</Button>
+      {/* whole-card animation */}
+      <motion.div
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 30, opacity: 0 }}
+        transition={{ duration: 1, ease: 'easeOut' }}
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle>Create a new account</CardTitle>
+            <CardDescription>
+              Enter your email below to create your new account
+            </CardDescription>
+          </CardHeader>
 
-                <FieldDescription className="text-center">
-                  Already have an account?{' '}
-                  <button onClick={switchToLogIn} className="underline">
-                    Log In
-                  </button>
-                </FieldDescription>
-              </Field>
-            </FieldGroup>
-          </form>
-        </CardContent>
-      </Card>
+          <CardContent>
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault()
+                const formData = new FormData(e.target as HTMLFormElement)
+                await signUpFn({
+                  data: {
+                    email: formData.get('email') as string,
+                    password: formData.get('password') as string,
+                  },
+                })
+                router.invalidate()
+              }}
+            >
+              <FieldGroup>
+                <Field>
+                  <FieldLabel htmlFor="email">Email</FieldLabel>
+                  <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </Field>
+                <Field>
+                  <div className="flex items-center">
+                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                  </div>
+                  <Input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                  />
+                </Field>
+                <Field>
+                  <Button type="submit">Sign Up</Button>
+
+                  <FieldDescription className="text-center">
+                    Already have an account?{' '}
+                    <button onClick={switchToLogIn} className="underline">
+                      Log In
+                    </button>
+                  </FieldDescription>
+                </Field>
+              </FieldGroup>
+            </form>
+          </CardContent>
+        </Card>
+      </motion.div>
     </div>
   )
 }
